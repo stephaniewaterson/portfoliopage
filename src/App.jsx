85c4efdc6b9960696c391f7b7357a076b6a395af
Stephanie Waterson <stephaniemariewaterson@gmail.com>
2025-05-23 16:39:41 +0100

    add scroll skill section

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import { a as web } from "@react-spring/web";
 import { easing } from "maath";
 import { useStore } from "../src/components/Store/Store";
 import { Overlay } from "./components/Overlay/Overlay";
+import { SkillsSection } from "./components/Interface/Interface";
 
 extend({ Overlay });
 
@@ -105,12 +106,14 @@ function App({ children }) {
         >
           click to open
         </web.h1>
+
         <ErrorBoundary>
           <Route path="/">
             <Canvas
               className="flex justify-center items-center h-screen w-screen"
               camera={{ position: [0, -3.5, 12.5] }}
               dpr={[1, 3]}
+              gl={{ antialias: false }}
             >
               <ScrollControls damping={0.1} pages={3}>
                 <ambientLight />
@@ -170,6 +173,7 @@ function App({ children }) {
                       />
                     )}
                   </Scroll>
+                  <SkillsSection />
                 </Suspense>
 
                 <directionalLight
@@ -202,3 +206,211 @@ function App({ children }) {
 }
 
 export default App;
+
+// import "./App.css";
+// import { Canvas, useFrame } from "@react-three/fiber";
+// import { Suspense, useState, useRef } from "react";
+// import { useErrorBoundary } from "use-error-boundary";
+// import { extend } from "@react-three/fiber";
+// import { Route } from "wouter";
+// import { useSpring } from "@react-spring/core";
+// import {
+//   OrbitControls,
+//   ContactShadows,
+//   MeshTransmissionMaterial,
+//   Text,
+//   ScrollControls,
+//   Scroll,
+//   useScroll,
+// } from "@react-three/drei";
+// import { Mac } from "../public/Mac-draco";
+// import { a as web } from "@react-spring/web";
+// import { easing } from "maath";
+// import { useStore } from "../src/components/Store/Store";
+// import { Overlay } from "./components/Overlay/Overlay";
+// import { SkillsSection } from "./components/Interface/Interface";
+
+// extend({ Overlay });
+
+// function App({ children }) {
+//   const { ErrorBoundary, didCatch, error } = useErrorBoundary();
+//   const [open, setOpen] = useState(false);
+//   const [hoveredState, setHoveredState] = useState(false);
+//   const props = useSpring({ open: Number(open) });
+
+//   function Selector({ children }) {
+//     const ref = useRef();
+//     const store = useStore();
+//     useFrame(({ viewport, camera, pointer }, delta) => {
+//       const { width, height } = viewport.getCurrentViewport(camera, [0, 0, 3]);
+//       easing.damp3(
+//         ref.current.position,
+//         [(pointer.x * width) / 2, (pointer.y * height) / 5, 5],
+//         store.openOverlay ? 0 : 0.1,
+//         delta
+//       );
+//       easing.damp3(
+//         ref.current.scale,
+//         store.openOverlay ? 4 : 0.01,
+//         store.openOverlay ? 0.5 : 0.2,
+//         delta
+//       );
+//       easing.dampC(
+//         ref.current.material.color,
+//         store.openOverlay ? "#f0f0f0" : "#ccc",
+//         0.1,
+//         delta
+//       );
+//     });
+//     return (
+//       <>
+//         <mesh ref={ref}>
+//           <circleGeometry args={[1, 64, 64]} />
+//           <MeshTransmissionMaterial
+//             samples={16}
+//             resolution={512}
+//             anisotropicBlur={0.1}
+//             thickness={0.1}
+//             roughness={0.4}
+//             toneMapped={true}
+//           />
+//         </mesh>
+//         <group
+//           onPointerOver={() => (store.openOverlay = true)}
+//           onPointerOut={() => (store.openOverlay = false)}
+//           onPointerDown={() => (store.openOverlay = true)}
+//           onPointerUp={() => (store.openOverlay = false)}
+//         >
+//           {children}
+//         </group>
+//       </>
+//     );
+//   }
+
+//   // if hovered state then show text
+
+//   if (hoveredState && open) {
+//   }
+
+//   return (
+//     <web.main
+//       style={{
+//         background: props.open.to([0, 1], ["#f0f0f0", "#ff5c5c"]),
+//       }}
+//     >
+//       <div
+//         style={{
+//           height: "100vh",
+//           width: "100vw",
+//           overflow: "hidden",
+//         }}
+//       >
+//         <web.h1
+//           style={{
+//             opacity: props.open.to([0, 1], [1, 0]),
+//             transform: props.open.to(
+//               (o) => `translate3d(-50%,${o * 50 - 100}px,0)`
+//             ),
+//           }}
+//         >
+//           click to open
+//         </web.h1>
+//         <ErrorBoundary>
+//           <Route path="/">
+//             <Canvas
+//               className="flex justify-center items-center h-screen w-screen"
+//               camera={{ position: [0, -3.5, 12.5] }}
+//               dpr={[1, 3]}
+//               gl={{ antialias: false }}
+//             >
+//               <ScrollControls damping={0.1} pages={3}>
+//                 <ambientLight />
+
+//                 <ContactShadows
+//                   resolution={512}
+//                   position={[0, -0.8, 0]}
+//                   opacity={1}
+//                   scale={10}
+//                   blur={2}
+//                   far={0.8}
+//                 />
+
+//                 <Suspense fallback={null}>
+//                   <group
+//                     rotation={[0, 0, 0]}
+//                     onClick={(e) => (e.stopPropagation(), setOpen(!open))}
+//                   >
+//                     <Selector>
+//                       <Scroll>
+//                         <Mac
+//                           rotation={[1.15, Math.PI, 1]}
+//                           open={open}
+//                           hinge={props.open.to([0, 1], [1.575, -0.425])}
+//                           onPointerEnter={() => setHoveredState(true)}
+//                         />
+//                       </Scroll>
+//                     </Selector>
+//                   </group>
+
+//                   <Scroll>
+//                     {open && (
+//                       <Text
+//                         style={{
+//                           position: "absolute",
+//                           top: "60vh",
+//                           left: "0.5em",
+//                         }}
+//                         className="name"
+//                         position={[2, 12, -7]}
+//                         fontSize={6}
+//                       >
+//                         Stephanie Waterson
+//                         <meshStandardMaterial
+//                           color="#ffffff"
+//                           toneMapped={false}
+//                         />
+//                       </Text>
+//                     )}
+//                     {hoveredState && open && (
+//                       <Overlay
+//                         style={{
+//                           position: "absolute",
+//                           top: "60vh",
+//                           left: "0.5em",
+//                         }}
+//                       />
+//                     )}
+//                   </Scroll>
+//                   {/* <SkillsSection /> */}
+//                 </Suspense>
+
+//                 <directionalLight
+//                   position={[0, 5, -2]}
+//                   scale={[3, 3, 3]}
+//                   intensity={Math.PI}
+//                   color="#FFFFFF"
+//                 />
+//                 <directionalLight
+//                   position={[2, 2, 2]}
+//                   scale={[3, 3, 3]}
+//                   intensity={Math.PI}
+//                   color="#FFFFFF"
+//                 />
+//                 {/* <OrbitControls /> */}
+//                 <ContactShadows
+//                   position={[0, -4.5, 0]}
+//                   opacity={0.4}
+//                   scale={20}
+//                   blur={1.75}
+//                   far={4.5}
+//                 />
+//               </ScrollControls>
+//             </Canvas>
+//           </Route>
+//         </ErrorBoundary>
+//       </div>
+//     </web.main>
+//   );
+// }
+
+// export default App;
--- src/components/Interface/Interface.jsx
+++ src/components/Interface/Interface.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { Html } from "@react-three/drei";
 
 const Section = (props) => {
   const { children } = props;
@@ -23,15 +24,138 @@ const Section = (props) => {
   );
 };
 
-export const Interface = () => {
+const skills = [
+  {
+    title: "Threejs / React Three Fiber",
+    level: 80,
+  },
+  {
+    title: "React / React Native",
+    level: 90,
+  },
+  {
+    title: "Nodejs",
+    level: 90,
+  },
+  {
+    title: "Typescript",
+    level: 60,
+  },
+  {
+    title: "3D Modeling",
+    level: 40,
+  },
+];
+const languages = [
+  {
+    title: "🇫🇷 French",
+    level: 100,
+  },
+  {
+    title: "🇺🇸 English",
+    level: 80,
+  },
+  {
+    title: "🇯🇵 Japanese",
+    level: 20,
+  },
+];
+
+export const SkillsSection = () => {
   return (
-    <div>
-      <AboutSection />
-      <SkillsSection />
+    <Html>
       <Section>
-        <h1>Projects</h1>
+        <motion.div whileInView={"visible"}>
+          <h2 className="text-5xl font-bold">Skills</h2>
+          <div className=" mt-8 space-y-4">
+            {skills.map((skill, index) => (
+              <div className="w-64" key={index}>
+                <motion.h3
+                  className="text-xl font-bold text-gray-800"
+                  initial={{
+                    opacity: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      opacity: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 1 + index * 0.2,
+                      },
+                    },
+                  }}
+                >
+                  {skill.title}
+                </motion.h3>
+                <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                  <motion.div
+                    className="h-full bg-indigo-500 rounded-full "
+                    style={{ width: `${skill.level}%` }}
+                    initial={{
+                      scaleX: 0,
+                      originX: 0,
+                    }}
+                    variants={{
+                      visible: {
+                        scaleX: 1,
+                        transition: {
+                          duration: 1,
+                          delay: 1 + index * 0.2,
+                        },
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+          <div>
+            <h2 className="text-5xl font-bold mt-10">Languages</h2>
+            <div className=" mt-8 space-y-4">
+              {languages.map((lng, index) => (
+                <div className="w-64" key={index}>
+                  <motion.h3
+                    className="text-xl font-bold text-gray-800"
+                    initial={{
+                      opacity: 0,
+                    }}
+                    variants={{
+                      visible: {
+                        opacity: 1,
+                        transition: {
+                          duration: 1,
+                          delay: 2 + index * 0.2,
+                        },
+                      },
+                    }}
+                  >
+                    {lng.title}
+                  </motion.h3>
+                  <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                    <motion.div
+                      className="h-full bg-indigo-500 rounded-full "
+                      style={{ width: `${lng.level}%` }}
+                      initial={{
+                        scaleX: 0,
+                        originX: 0,
+                      }}
+                      variants={{
+                        visible: {
+                          scaleX: 1,
+                          transition: {
+                            duration: 1,
+                            delay: 2 + index * 0.2,
+                          },
+                        },
+                      }}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </motion.div>
       </Section>
-      <ContactSection />
-    </div>
+    </Html>
   );
 };
